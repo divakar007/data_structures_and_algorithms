355c33727f1c3a0a8c06f9390fc8c9c7e624964c
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-21 19:31:47 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.00%) - LeetSync

---
+++ 1295-minimum-garden-perimeter-to-collect-enough-apples/minimum-garden-perimeter-to-collect-enough-apples.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minimumPerimeter(self, neededApples):
+        """
+        :type neededApples: int
+        :rtype: int
+        """
+
+        def find_val(n):
+            return (n*(n+1)*(2*n+1))*2
+        
+        s=1
+        e=10**6
+
+        while s<e:
+            m = (s+e)//2
+            val = find_val(m)
+            if val >= neededApples:
+                e = m
+            else:
+                s= m+1
+
+        return s*8
+
