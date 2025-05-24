6123fa35430b8137cf7fb18d27530ea0d63ecc9c
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-23 21:06:17 -0400

    Time: 731 ms (40.77%) | Memory: 13.3 MB (12.31%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def maximumDetonation(self, bombs):
+        """
+        :type bombs: List[List[int]]
+        :rtype: int
+        """
+
+        def distance(bomb1, bomb2):
+            x1, y1, r1 = bomb1
+            x2, y2, r2 = bomb2
+            return ((x2-x1)**2 + (y1-y2)**2)**0.5
+
+        graph = defaultdict(list)
+
+        for i in range(len(bombs)):
+            for j in range(len(bombs)):
+                if i == j:
+                    continue
+                if distance(bombs[i], bombs[j]) <= bombs[i][2]:
+                    graph[i].append(j)
+        
+        nodes = list(range(len(bombs)))
+
+        
+        print(graph)
+
+        def dfs(node, visited):
+            visited.add(node)
+            count = 0
+            for nxt in graph[node]:
+                if nxt not in visited:
+                    count += (1 + dfs(nxt, visited))
+            return count
+
+        maxCount = 0
+        for i in range(len(bombs)):
+            maxCount = max(maxCount, 1+dfs(i,set()))
+        
+        return maxCount
+
+
+
+                
+    
+    
+        
