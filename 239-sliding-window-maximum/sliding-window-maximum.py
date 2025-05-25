3edf42d08d52b2ad475e25e26f6c1a5fdfa35cf6
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-25 17:55:16 -0400

    Time: 227 ms (98.85%) | Memory: 29.3 MB (17.40%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -5,32 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        queue = deque([])
+
+        for i in range(k):
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+        result = [nums[queue[0]]]
+
         n = len(nums)
-        left = 0
-        right = 0
-        dq = deque([])
-        res = []
-        while(right < n):
-            if len(dq) == 0:
-                dq.append(right)
-            else:
-                while(nums[dq[-1]] < nums[right]):
-                    dq.pop()
-
-                    if len(dq) == 0:
-                        break
-                dq.append(right)
-                
-            
-            if right - left + 1 == k:
-                res.append(nums[dq[0]])
-                left += 1
-                if dq[0] < left:
-                    dq.popleft()
-
-            right += 1
-        
-        return res
+        for i in range(k, n):
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+            if queue[0] <= i - k:
+                queue.popleft() 
+            result.append(nums[queue[0]])
+        return result
 
 
 
