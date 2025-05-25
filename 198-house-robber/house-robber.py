d76a512ca55f8122c658a24a600023aaf2047522
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-25 18:10:16 -0400

    Time: 4 ms (5.59%) | Memory: 12.5 MB (53.92%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[1], nums[0])
+
+        for i in range(2, n):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        print(dp)
+        return dp[n-1]
+        
+        
