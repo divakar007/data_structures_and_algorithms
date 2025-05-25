0bdaa968caf1ec6d5d3f01ad38d7b7df39e91cf6
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-25 18:11:41 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (14.59%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -9,13 +9,13 @@ class Solution(object):
         if n == 1:
             return nums[0]
 
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], nums[0])
+        prev = nums[0]
+        curr = max(nums[1], nums[0])
 
         for i in range(2, n):
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        print(dp)
-        return dp[n-1]
+            nxt = max(prev + nums[i], curr)
+            curr, prev = nxt, curr
+        
+        return curr
         
         
