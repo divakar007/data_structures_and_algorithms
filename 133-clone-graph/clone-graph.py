7d2e493c2dcc50b0512dd98db7d5226807ee9fe6
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-25 11:35:02 -0400

    Time: 34 ms (64.26%) | Memory: 12.7 MB (60.31%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -1,20 +1,36 @@
-from collections import deque
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 
+from collections import deque
 class Solution(object):
     def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
         if not node:
             return None
-
+        
         newNodes = {}
-        dq = deque([node])
-        newNodes[node] = Node(node.val)  # Create the first node
+        dq = deque([])
+        dq.append(node)
 
+        newNodes[node] = Node(node.val)
         while dq:
             curr = dq.popleft()
             for nei in curr.neighbors:
                 if nei not in newNodes:
-                    newNodes[nei] = Node(nei.val)
+                    newNodes[nei] = Node(nei.val, None)
                     dq.append(nei)
                 newNodes[curr].neighbors.append(newNodes[nei])
-
+            
         return newNodes[node]
+
+            
+
+        
