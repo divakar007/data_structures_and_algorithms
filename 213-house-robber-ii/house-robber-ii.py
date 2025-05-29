5799f82264bcdb6f6142e76b4da2191684a7cf8e
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-29 11:33:24 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.61%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -5,23 +5,26 @@ class Solution(object):
         :rtype: int
         """
     
-        n = len(nums)-1
+        n = len(nums)
 
-        if n == 0:
+        if n == 1:
             return nums[0]
         
-        def robHouseUtil(index, arr, memo):
-            if index >= n:
-                return 0
-            if memo[index] != -1:
-                return memo[index]
-            pick = arr[index] + robHouseUtil(index+2, arr, memo)
-            notPick = robHouseUtil(index+1, arr, memo)
-            memo[index] = max(pick, notPick)
-            
-            return memo[index]
+        def dpSolution(arr):
+            n = len(arr)
+            dp = [0] * (n+1)
+            dp[1] = arr[0]
 
-        return max(robHouseUtil(0,nums[1:], [-1] * n), robHouseUtil(0, nums[:-1], [-1] * n))
+            for index in range(2, n+1):
+                pick = arr[index-1] + dp[index-2]
+                notPick = dp[index-1]
+                dp[index] = max(pick, notPick)
+                
+            return dp[n]
+        
+        return max(dpSolution(nums[1:]), dpSolution(nums[:-1]))
+        
+        
 
 
         
