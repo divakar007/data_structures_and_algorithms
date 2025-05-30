e208d740916dd6345ee945484bbe52908750b9da
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-30 18:40:43 -0400

    Time: 51 ms (33.93%) | Memory: 13.3 MB (22.52%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -15,6 +15,5 @@ class Solution(object):
         for row in range(rows-2, -1, -1):
             for col in range(cols):
                     min_sum_dp[row][col] = matrix[row][col] +  min([min_sum_dp[row+1][col+y] if 0 <= col+y < cols else float('inf') for y in [-1, 0, 1]])
-        print(min_sum_dp)
         return min(min_sum_dp[0])
 
