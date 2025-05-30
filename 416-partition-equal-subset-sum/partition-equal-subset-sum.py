bde90d169063355a2b817fff8b7b157e7e716820
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-05-30 14:19:53 -0400

    Time: 392 ms (83.39%) | Memory: 12.9 MB (79.28%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,29 +1,19 @@
 class Solution(object):
     def canPartition(self, nums):
-        if sum(nums) % 2:
+        total = sum(nums)
+        if total % 2:
             return False
-        
-        target = sum(nums) // 2
-        n = len(nums)
-        memo = {}  # key: (i, amountLeft), value: True/False
+        target = total // 2
 
-        def dfs(i, rem):
-            # base cases
-            if rem == 0:
+        dp = [False] * (target+1)
+        dp[0] = True
+
+        for num in nums:
+            # walk amounts *down* from target to num
+            for amt in range(target, num-1, -1):
+                if dp[amt - num]:
+                    dp[amt] = True
+            if dp[target]:
                 return True
-            if i == n or rem < 0:
-                return False
 
-            # memo check
-            key = (i, rem)
-            if key in memo:
-                return memo[key]
-
-            # choice: take nums[i] or skip it
-            take = dfs(i+1, rem - nums[i])
-            skip = dfs(i+1, rem)
-
-            memo[key] = take or skip
-            return memo[key]
-
-        return dfs(0, target)
+        return dp[target]
