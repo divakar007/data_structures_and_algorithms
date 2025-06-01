4427bf3b4e3fa23de68759caa6de536ffd1b3f4a
Divakara Rao Annepu <47806465+divakar007@users.noreply.github.com>
2025-06-01 15:17:13 -0400

    Time: 3128 ms (63.64%) | Memory: 43.3 MB (18.18%) - LeetSync

---
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minimumArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+
+        m = len(grid)
+        n = len(grid[0])
+        min_i, min_j, max_i, max_j = m, n, 0, 0
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]:
+                    min_i = min(min_i, row)
+                    min_j = min(min_j, col)
+                    max_i = max(max_i, row)
+                    max_j = max(max_j, col)
+        print(max_i, min_i, max_j, min_j)
+        return (max_i-min_i+1) * (max_j-min_j+1)
+
+                    
